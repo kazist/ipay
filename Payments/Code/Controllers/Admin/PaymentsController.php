0c355a3c7f869cf926c018a0871156156f19accd
dedanirungu <dedanirungu@gmail.com>
2017-08-16 10:06:35 +0300

    Adding Payment and implementing General Payment processes

--- Payments/Code/Controllers/Admin/PaymentsController.php
+++ Payments/Code/Controllers/Admin/PaymentsController.php
@@ -8,7 +8,7 @@
  */
 
 /**
- * Description of IpayController
+ * Description of PaymentsController
  *
  * @author sbc
  */
--- Payments/Code/Models/PaymentsModel.php
+++ Payments/Code/Models/PaymentsModel.php
@@ -24,6 +24,12 @@ class PaymentsModel extends BasePaymentsModel {
 
     public $code = '';
 
+    public function appendSearchQuery($query) {
+
+        $this->ingore_search_query = true;
+        return parent:: appendSearchQuery($query);
+    }
+
     public function notificationTransaction($payment_id) {
         $this->processIpay($payment_id);
     }
